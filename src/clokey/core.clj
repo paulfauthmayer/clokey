bb1338081e43760790078bf0aa0edd570cfd277a
Paul Fauth-Mayer <paulfauthmayer@gmail.com>
2017-12-20 12:43:53 +0100

    initiated lein app

---
+++ src/clokey/core.clj
@@ -0,0 +1,7 @@
+(ns clokey.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
---
+++ test/clokey/core_test.clj
@@ -0,0 +1,7 @@
+(ns clokey.core-test
+  (:require [clojure.test :refer :all]
+            [clokey.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
