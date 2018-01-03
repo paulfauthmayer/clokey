2e4d769b4fef855c41dd2bf22a4d8b9dc0875ca7
Paul Fauth-Mayer <paulfauthmayer@gmail.com>
2018-01-03 12:53:21 +0100

    function outline

--- src/clokey/core.clj
+++ src/clokey/core.clj
@@ -5,3 +5,45 @@
   "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World!"))
+
+;; Core Functionality
+
+; CRUD + Validation
+
+(defn create-password)
+
+(defn read-password)
+
+(defn update-password)
+
+(defn delete-passward)
+
+(defn valid?)
+
+
+; User Management
+{:name "Alex da G" :mpw "encrypted-pw"
+ :accounts 
+ [{:source "facebook.com"
+   :username "bigtittyanimegurl"
+   :pw "encrypted-pw"}
+  {}
+  {}]}
+
+(defn create-user)
+
+(defn get-user)
+
+(defn delete-user)
+
+(defn update-user)
+
+; Security
+
+(defn encrypt)
+
+(defn decrypt)
+
+(defn authenticate)
+
+(defn authenticated?)
