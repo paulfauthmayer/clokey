0374c439db753d4e92b5143b034f78fcfe3b412d
Paul Fauth-Mayer <paulfauthmayer@gmail.com>
2018-01-04 15:44:58 +0100

    included proto repl

--- project.clj
+++ project.clj
@@ -4,7 +4,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [crypto-password "0.2.0"]]
+                 [crypto-password "0.2.0"]
+                 [proto-repl "0.3.1"]]
   :main ^:skip-aot clokey.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
