16ebbb02ab834b0fc450f753d9f845f8fb5e5b63
Paul Fauth-Mayer <paulfauthmayer@gmail.com>
2018-01-23 17:19:39 +0100

    mongo db

--- src/clokey/handler.clj
+++ src/clokey/handler.clj
@@ -4,20 +4,33 @@
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
             [clojure.string :as string]
             [clokey.user :as user]
-            [clokey.utils :as utils]))
+            [clokey.utils :as utils]
+            [monger.core :as mg]
+            [monger.collection :as mc])
+  (:import [com.mongodb MongoOptions ServerAddress]
+           org.bson.types.ObjectId))
+
+;; initialize mongodb
+
+(let [conn (mg/connect)
+      db   (mg/get-db conn "clokey-db")]
+
+;; define routes
+  (defroutes app-routes
+    (GET "/hello-there" [] "General Kenobi")
+    (GET "/user/:id" [id]
+      (str "<h1>HI USER NR " id "</h1>"))
+    (GET "/encrypt/:pw" [pw]
+      (utils/encrypt pw))
+    (POST "/" request
+      (str request))
+    (GET "/foobar" [x y & z]
+      (str x ", " y ", " z))
+    (GET "/create-user" [username mpw]
+      (let [new-user (user/create-user username mpw)]
+        (mc/insert-and-return db "users" new-user)))
+    (route/not-found "Not Found"))
 
 
-(defroutes app-routes
-  (GET "/hello-there" [] "General Kenobi")
-  (GET "/user/:id" [id]
-    (str "<h1>HI USER NR " id "</h1>"))
-  (GET "/encrypt/:pw" [pw]
-    (utils/encrypt pw))
-  (POST "/" request
-    (str request))
-  (GET "/foobar" [x y & z]
-    (str x ", " y ", " z))
-  (route/not-found "Not Found"))
-
-(def app
-  (wrap-defaults app-routes site-defaults))
+  (def app
+    (wrap-defaults app-routes site-defaults)))
